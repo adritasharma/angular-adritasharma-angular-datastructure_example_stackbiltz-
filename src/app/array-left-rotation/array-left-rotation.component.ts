65c3e4d0b0255d32e633b3af880240cefb854f15
adritasharma <adritasharma.developer@gmail.com>
2020-04-25 22:04:59 +0530

    ArrayLeftRotation Working

--- src/app/array-left-rotation/array-left-rotation.component.ts
+++ src/app/array-left-rotation/array-left-rotation.component.ts
@@ -6,31 +6,41 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./array-left-rotation.component.css"]
 })
 export class ArrayLeftRotationComponent implements OnInit {
-  
   arr = [1, 2, 3, 4, 5];
-  rotation2 = [3, 4, 5, 1, 2];
-  rotation4 = [5, 1, 2, 3, 4];
+
   output = [];
 
-  // rotation2
-  // 0=2
-  // 1=3
-  // 2=4
+  // rotation = 2
 
-  // 3=0
-  // 4=1
+  // rotation2 = [3, 4, 5, 1, 2];
+  //==========================
 
-  // rotation4
+  // 0=>2
+  // 1=>3
+  // 2=>4
+
+  // Here right = left + 2; i.e left  + d
+  //========================
+  // 3=>0
+  // 4=>1
+  //Here right = left -3 ; i.e left  - (n-d)
+
+  // 3 iteration in 1st condition. i-e (n-d)
+
+  // rotation = 4
+
+  rotation4 = [5, 1, 2, 3, 4];
+
   // 0 =4
 
-  // 1= 0
-  // 2=1
-  // 3=2
-  // 4 =3
+  // 1=>0
+  // 2=>1
+  // 3=>2
+  // 4=>3
 
-  constructor() {
-    var n = 5;
-    var d = 2;
+  rotate(d) {
+    this.output = [];
+    var n = this.arr.length;
     for (var i = 0; i < n; i++) {
       if (i < n - d) {
         this.output[i] = this.arr[i + d];
